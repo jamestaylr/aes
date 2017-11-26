ecece8c5bcecd622c4d8d10348013b5847b26358
James Taylor <jamestaylr@gmail.com>
2017-11-25 22:05:41 -0500

    Test FiniteField struct overloaded arithmetic operations

---
+++ spec/aes/finite_field_spec.cr
@@ -0,0 +1,26 @@
+require "../../spec_helper"
+
+describe AES::FiniteField do
+  describe "#+" do
+    it "should add" do
+      a = AES::FiniteField.new(0b01010011)
+      b = AES::FiniteField.new(0b11001010)
+      (a + b).should eq AES::FiniteField.new(0b10011001)
+    end
+  end
+
+  describe "#-" do
+    it "should subtract" do
+      a = AES::FiniteField.new(0b01010011)
+      b = AES::FiniteField.new(0b11001010)
+      (a + b).should eq AES::FiniteField.new(0b10011001)
+    end
+  end
+  describe "#*" do
+    it "should multiply" do
+      a = AES::FiniteField.new(0b111)
+      b = AES::FiniteField.new(0b101)
+      (a * b).should eq AES::FiniteField.new(0b11011)
+    end
+  end
+end
