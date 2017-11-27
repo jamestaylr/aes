cd7583f94dded134d8de69010e55d68ab3864ffd
James Taylor <jamestaylr@gmail.com>
2017-11-27 11:26:55 -0500

    Remove modulus operator from FiniteField

--- src/aes/finite_field.cr
+++ src/aes/finite_field.cr
@@ -49,23 +49,5 @@ module AES
       end
       FiniteField.new(p)
     end
-
-    private def degree(n)
-      n.to_s(2).chars.size - 1
-    end
-
-    def %(other)
-      dv = degree(@value)
-      dc = degree(other.value)
-
-      p = @value
-      (0..dv - dc).reverse_each do |i|
-        # TODO guard against timing attacks
-        if p.to_u16[i + dc] != 0
-          p = (other.value << i) ^ p
-        end
-      end
-      FiniteField.new(p)
-    end
   end
 end
