a623397b89ca9ed91e3856289c4c9481e09463ca
James Taylor <jamestaylr@gmail.com>
2017-12-15 17:02:49 -0500

    Refactor process function and add decryption tests

--- src/aes/process.cr
+++ src/aes/process.cr
@@ -11,55 +11,56 @@ module AES
       plaintext.each_slice(16) { |j| blks << j }
 
       init_vector = Array(Int32).new(16, 0)
+      expanded_key = key_expansion(key)
       blks.map do |blk|
         # TODO create block generator
         blk.concat(blk.size != 16 ? Array(Int32).new(size = 16 - blk.size,
           value = 0) : [] of Int32)
 
-        if @mode.cbc? && @process.encrypt?
-          blk = blk.zip(init_vector).map { |j, k| j ^ k }
-        end
-
         if @process.encrypt?
-          processed = encrypt_block(blk.clone, key)
+          # Perform encryption process
+          if @mode.cbc?
+            blk = blk.zip(init_vector).map { |j, k| j ^ k }
+          end
+          processed = encrypt_block(blk.clone, expanded_key)
+          if @mode.cbc?
+            init_vector = processed
+          end
         else
-          processed = decrypt_block(blk.clone, key)
+          # Perform decryption process
+          processed = decrypt_block(blk.clone, expanded_key)
+          if @mode.cbc?
+            processed = processed.zip(init_vector).map { |j, k| j ^ k }
+            init_vector = blk
+          end
         end
 
-        if @mode.cbc? && @process.encrypt?
-          init_vector = processed
-        elsif @mode.cbc? && @process.decrypt?
-          processed = processed.zip(init_vector).map { |j, k| j ^ k }
-          init_vector = blk
-        end
         processed
       end.flatten
     end
 
-    def decrypt_block(plaintext : Array(Int), key : Array(Int))
-      expanded = key_expansion(key)
-      state = add_round_key(plaintext, expanded, @num_rounds)
+    def decrypt_block(plaintext : Array(Int), expanded_key : Array(Int))
+      state = add_round_key(plaintext, expanded_key, @num_rounds)
 
       (1...@num_rounds).reverse_each do |i|
         # TODO OOP or functional method chaining
         state = sub_bytes(shift_rows(state))
-        state = mix_columns(add_round_key(state, expanded, i))
+        state = mix_columns(add_round_key(state, expanded_key, i))
       end
       state = sub_bytes(shift_rows(state))
-      add_round_key(state, expanded, 0)
+      add_round_key(state, expanded_key, 0)
     end
 
-    def encrypt_block(plaintext : Array(Int), key : Array(Int))
-      expanded = key_expansion(key)
-      state = add_round_key(plaintext, expanded, 0)
+    def encrypt_block(plaintext : Array(Int), expanded_key : Array(Int))
+      state = add_round_key(plaintext, expanded_key, 0)
 
       (1...@num_rounds).each do |i|
         # TODO OOP or functional method chaining
         state = shift_rows(sub_bytes(state))
-        state = add_round_key(mix_columns(state), expanded, i)
+        state = add_round_key(mix_columns(state), expanded_key, i)
       end
       state = shift_rows(sub_bytes(state))
-      add_round_key(state, expanded, @num_rounds)
+      add_round_key(state, expanded_key, @num_rounds)
     end
   end
 end
